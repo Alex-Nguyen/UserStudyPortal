75dd74d7a6e7f63bcab51ff2121c9578d81294c6
Vinh T. Nguyen <vinhnt@tnu.edu.vn>
2019-12-01 21:05:01 -0600

    upload

---
+++ js/app.js
@@ -0,0 +1,400 @@
+const database = firebase.database();
+class UserStudy {
+    constructor(params){
+        this.maxTimeToFinishPage = params.maxTimeToFinishPage;
+        this.maxTimeToFinish = params.maxTimeToFinish;
+    }
+    init(){
+        let _this = this;
+        let uuid = this.uuidv4();
+        let json = this.json();
+        _this.survey = new Survey.Model(json);
+        _this.survey
+            .onComplete
+            .add(function (result) {
+                let val = _this.survey.currentPage.getValue();
+                let qname = Object.keys(val)[0];
+                let qanwer = val[qname];
+
+                database.ref(`${uuid}/${qname}`).set({answer:qanwer, timespent:_this.survey.currentPage.timeSpent });
+            });
+        _this.survey.onCurrentPageChanging.add(function (sender, options) {
+            // console.log(`Page changing: `+ survey.currentPage.timeSpent)
+            let val = _this.survey.currentPage.getValue();
+            let qname = Object.keys(val)[0];
+            let qanwer = val[qname];
+
+            database.ref(`${uuid}/${qname}`).set({answer:qanwer, timespent:_this.survey.currentPage.timeSpent });
+        });
+        $("#surveyElement").Survey({
+            model: _this.survey
+        });
+    }
+    json(){
+        return {
+            title: "Human Computer Interaction - User study",
+            showProgressBar: "top",
+            showTimerPanel: "top",
+            maxTimeToFinishPage: this.maxTimeToFinishPage,
+            maxTimeToFinish: this.maxTimeToFinish,
+            firstPageIsStarted: true,
+            startSurveyText: "Start Survey",
+            pages: [
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            html: "You are about to start user study by evaluating visualization design. <br/>You have 2 minutes for every page and 36 minutes for the whole survey of 18 questions.<br/>Please click on <b>'Start Survey'</b> button when you are ready."
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' height='400px /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q1",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q2.png' width='100%' height='400px /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q2",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' height='400px /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q3",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q4",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q5",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q6",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q7",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q8",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q9",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q10",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q11",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q12",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q13",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q14",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q15",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q16",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q17",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            type: "html",
+                            name: "info",
+                            html: "<table><body><row><td><img src='images/q1.png' width='100%' height='400px' /></td><td style='padding:20px'></td></row></body></table>"
+                        },
+                        {
+                            type: "radiogroup",
+                            name: "q18",
+                            title: "From the design above, which group shows most sudden change over time?",
+                            choices: [
+                                "Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"
+                            ],
+                            correctAnswer: "Group 2"
+                        }
+                    ]
+                },
+                {
+                    questions: [
+                        {
+                            "type": "comment",
+                            "name": "comment",
+                            "title":"What is your comment about each visualization type?"
+                        }
+                    ],
+                }
+            ],
+            completedHtml: "<h4>You have finished user study section, thank you very much for your time.</h4>"
+        };
+    }
+    uuidv4(){
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+}
+const params = {
+    maxTimeToFinishPage: 120, //in seconds
+    maxTimeToFinish: 2160 //in seconds
+}
+
+let userstudy = new UserStudy(params);
+userstudy.init();
